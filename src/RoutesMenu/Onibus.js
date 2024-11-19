349b95a2ef299415b208523d10cebe0e77ecf34a
icassimiro <icassimiro@hotmail.com>
2024-11-19 16:24:23 -0300

    css

--- src/RoutesMenu/Onibus.js
+++ src/RoutesMenu/Onibus.js
@@ -23,7 +23,57 @@ export default function Onibus() {
                     <h3 className="cond">Condição</h3>
                     <h3 className="cond1">Completamente revisado</h3>
 
-                    <div className="divbtn"><a target="_blank" href="https://drive.google.com/file/d/1TPK77CnAP2hIfbqya2T_g73ABn3RkGyn/view?usp=sharing">
+                    <div className="divbtn"><a target="_blank" href="https://drive.google.com/file/d/11G2uTizlkukljHAcO6Q5W2YCwxmz-kQn/view?usp=sharing">
+                        <button className="btn">VISUALIZAR VEÍCULO</button>
+                    </a>
+                        <a target="_blank" href="https://wa.me/31997188046">
+                            <button className="btn1"><IoLogoWhatsapp className="wppi" /></button>
+                        </a>
+                    </div>
+
+                </div>
+                <img className="bus" src={bus}></img>
+            </div>
+            <div className="list">
+                <div className="infos">
+                    <img className="iconb" src={benz}></img>
+                    <h2 className="type">Ônibus Mercedes & Caio</h2>
+
+                    <h2 className="ano">Ano</h2>
+                    <h2 className="ano1">2012 & 2013</h2>
+                    <h3 className="km">KM</h3>
+                    <h3 className="km1">XX.XXXX</h3>
+                    <h3 className="cor">Cor</h3>
+                    <h3 className="cor1">Verde & Azul  </h3>
+                    <h3 className="cond">Condição</h3>
+                    <h3 className="cond1">Completamente revisado</h3>
+
+                    <div className="divbtn"><a target="_blank" href="https://drive.google.com/file/d/11G2uTizlkukljHAcO6Q5W2YCwxmz-kQn/view?usp=sharing">
+                        <button className="btn">VISUALIZAR VEÍCULO</button>
+                    </a>
+                        <a target="_blank" href="https://wa.me/31997188046">
+                            <button className="btn1"><IoLogoWhatsapp className="wppi" /></button>
+                        </a>
+                    </div>
+
+                </div>
+                <img className="bus" src={bus}></img>
+            </div>
+            <div className="list">
+                <div className="infos">
+                    <img className="iconb" src={benz}></img>
+                    <h2 className="type">Ônibus Mercedes & Caio</h2>
+
+                    <h2 className="ano">Ano</h2>
+                    <h2 className="ano1">2012 & 2013</h2>
+                    <h3 className="km">KM</h3>
+                    <h3 className="km1">XX.XXXX</h3>
+                    <h3 className="cor">Cor</h3>
+                    <h3 className="cor1">Verde & Azul  </h3>
+                    <h3 className="cond">Condição</h3>
+                    <h3 className="cond1">Completamente revisado</h3>
+
+                    <div className="divbtn"><a target="_blank" href="https://drive.google.com/file/d/11G2uTizlkukljHAcO6Q5W2YCwxmz-kQn/view?usp=sharinggit">
                         <button className="btn">VISUALIZAR VEÍCULO</button>
                     </a>
                         <a target="_blank" href="https://wa.me/31997188046">
